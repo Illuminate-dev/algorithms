2553d40eb15b6ed7957989ecd843e46b0d350f67
Henry Beveridge <70719277+Illuminate-dev@users.noreply.github.com>
2023-04-20 14:56:05 +0000

    shell sort

--- src/sorting.rs
+++ src/sorting.rs
@@ -67,6 +67,26 @@ pub fn selection_sort<T: Ord>(arr: &mut [T]) {
     }
 }
 
+pub fn shell_sort<T: Ord>(arr: &mut [T]) {
+    fn h_sort<T: Ord>(arr: &mut [T], h: usize) {
+        for i in h..arr.len() {
+            let mut j = i; 
+            while j >= h && arr[j] < arr[j-h] {
+                arr.swap(j, j-h);
+            }
+        }
+    }    
+
+    let mut h = 1;
+    while h < arr.len() {
+        h = 3 * h + 1;
+    }
+    while h > 0 {
+        h = h / 3;
+        h_sort(arr, h);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
